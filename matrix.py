27af54984076ab779710a62ff5c058f5ad1a5614
Grigorov- <Grigorov-@users.noreply.github.com>
2015-06-08 19:20:56 +0300

    create matrix.py

---
+++ matrix.py
@@ -0,0 +1,52 @@
+class Matrix:
+	def __init__(self, m, n, *args):
+		pass
+
+	def sum(self, other):
+		pass
+
+	def diff(self, other):
+		pass 
+
+	def mult(self, other):
+		pass
+
+	def division(self, other):
+		pass
+
+	def pow(self, power):
+		pass
+
+	def inverse(self):
+		pass
+
+	def inversed(self):
+		pass
+	
+	def  __str__ (self):
+		pass
+
+	def determinant(self):
+		pass
+
+	def __get__(self):
+		pass
+
+	def __set__(self, i, j, value):
+		pass
+
+	def is_symmetric(self):
+		pass
+
+	def is_orthogonal(self):
+		pass
+
+	def is_diagonal(self):
+		pass
+
+	def m(self):
+		pass
+
+	def n(self):
+		pass
+		
